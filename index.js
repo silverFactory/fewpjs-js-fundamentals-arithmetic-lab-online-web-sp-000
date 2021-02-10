6b79751c54fa95a4633c0cbd7e596b98fcc59977
Will Studabaker <will.studabaker@gmail.com>
2021-02-10 11:16:40 -0500

    commit tree 1c064a7230b21bb2ae5eec092c49a4c6f56a63be

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 20
+let num4 = 8
+let multiply = num1 * num2
+let random = Math.random()
+let mod = num3 % num4
+let max = Math.max(5, 10, 15, 20)
